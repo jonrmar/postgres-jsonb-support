01d3b17e8cae238a1887c2e1e9be6969245bc9fc
Jonathan R. Martinelli <jonathan@ubots.com.br>
2024-05-16 09:40:52 -0300

    test coderabbit

--- json-as-document-db/src/main/java/dao/PSQLJsonBException.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-public class PSQLJsonBException extends Exception {
-    public PSQLJsonBException(String message){
-        super(message);
-    }
-}
--- json-as-document-db/src/main/java/jdbc/ConnectionException.java
+++
@@ -1,8 +0,0 @@
-package jdbc;
-
-public class ConnectionException extends Exception {
-
-   public ConnectionException(String message) {
-        super(message);
-    }
-}
